056be2f3e21810414fb4364fbde286e4b498d7a5
Viacheslav Ivanov <tristandtm@gmail.com>
2021-12-10 16:52:07 +0300

    refactoring

--- src/main/kotlin/ru/cib/eternalsfedu/domain/News.kt
+++ src/main/kotlin/ru/cib/eternalsfedu/domain/News.kt
@@ -1,12 +1,19 @@
 package ru.cib.eternalsfedu.domain
 
+import org.hibernate.annotations.CreationTimestamp
+import java.util.*
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table("news")
 data class News(
-    var id: Long? = null,
     var title: Long? = null,
-    var
-)
+    var text: String? = null,
+    @field:CreationTimestamp
+    var creationTime: Date? = null,
+    var heatCounter: Long? = null,
+    var heatExpirationTime: Date? = null
+) : Domain()
